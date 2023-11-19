dff2470e93b34508f5b8d0d9a928ad30e09b086b
TatendaTorerwa <ttorerwa01@gmail.com>
2023-11-19 05:08:58 -0800

    0. Bubble sort.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,12 +14,15 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t p, j, k;
 	int temp;
+	int flag;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (p = 0; p < size - 1; p++)
 	{
+		flag = 0;
+
 		for (j = 0; j < size - 1 - p; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -27,6 +30,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				flag = 1;
 
 				for (k = 0; k < size; k++)
 				{
@@ -34,6 +38,10 @@ void bubble_sort(int *array, size_t size)
 				}
 				printf("\n");
 			}
+		}
+		if (flag == 0)
+		{
+			break;
 		}
 	}
 }
