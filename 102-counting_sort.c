95c249707f3cd3718d373880a4760620de44084d
TatendaTorerwa <ttorerwa01@gmail.com>
2023-11-21 05:43:58 -0800

    6. Counting sort.

---
+++ 102-counting_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+/**
+ *  counting_sort - Sorts an array using counting sort.
+ *
+ *  @array: The array to be sorted.
+ *
+ *  @size: The size of the array to be sorted.
+ *
+ *  Description: Returns the counting array after setting it up
+ */
+void counting_sort(int *array, size_t size)
+{
+    size_t i;
+    int max_element = array[0];
+    int *count, *count_b;
+
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > max_element)
+            max_element = array[i];
+    }
+
+    count = (int *)malloc((max_element + 1) * sizeof(int));
+    count_b = (int *)malloc(size * sizeof(int));
+
+    for (i = 0; i <= (size_t)max_element; i++)
+    {
+        count[i] = 0;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        count[array[i]]++;
+    }
+
+    for (i = 1; i <= (size_t)max_element; i++)
+    {
+        count[i] = count[i] + count[i - 1];
+    }
+    print_array(count, max_element + 1);
+
+    for (i = size - 1; i < size; i--)
+    {
+        count_b[--count[array[i]]] = array[i];
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        array[i] = count_b[i];
+    }
+
+    free(count);
+    free(count_b);
+}
